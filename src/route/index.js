dc30933bcfc388bd723eccc88e02e0c6a2b8fe63
Melzarok <timurmilutin727@gmail.com>
2024-07-31 22:52:57 +0300

    created user

--- src/route/index.js
+++ src/route/index.js
@@ -12,16 +12,46 @@ class User {
     this.id = new Date().getTime()
   }
 
+  verifyPassword = (password) => this.password === password
+
   static add = (user) => {
     this.#list.push(user)
   }
 
-  static getList = () => {
-    this.#list
+  static getList = () => this.#list
+
+  static getById = (id) => {
+    return this.#list.find((user) => user.id === id)
   }
 
-  static getById(id) {
-    this.#list.find((user) => user.id === id)
+  static deleteById = (id) => {
+    const index = this.#list.findIndex(
+      (user) => user.id === id,
+    )
+
+    if (index !== -1) {
+      this.#list.splice(index, 1)
+      return true
+    } else {
+      return false
+    }
+  }
+
+  static updateById = (id, data) => {
+    const user = this.getById(id)
+
+    if (user) {
+      this.update(user, data)
+      return true
+    } else {
+      return false
+    }
+  }
+
+  static update = (user, { email }) => {
+    if (email) {
+      user.email = email
+    }
   }
 }
 // ================================================================
@@ -61,17 +91,31 @@ router.post('/user-create', function (req, res) {
 router.get('/user-delete', function (req, res) {
   const { id } = req.query
 
-  console.log(id)
-
-  const user = User.getById(id)
-
-  if (user) {
-    console.log('!!!!!!!!!!!!!!!!!!!!!')
-  }
+  User.deleteById(Number(id))
 
   res.render('success-info', {
     style: 'success-info',
     info: 'Користувача видалено',
+  })
+})
+
+router.post('/user-update', function (req, res) {
+  const { email, password, id } = req.body
+
+  const user = User.getById(Number(id))
+
+  let result = false
+
+  if (user.verifyPassword(password)) {
+    User.update(user, { email })
+    result = true
+  }
+
+  res.render('success-info', {
+    style: 'success-info',
+    info: result
+      ? 'Емайл пошта оновлена!'
+      : 'Сталася помилка!',
   })
 })
 
