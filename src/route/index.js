b21de1af2b8918e28626066cec3a86e83d38f746
Melzarok <timurmilutin727@gmail.com>
2024-08-08 18:18:06 +0300

    problem

--- src/route/index.js
+++ src/route/index.js
@@ -51,8 +51,38 @@ class Product {
 
 class Purchase {
   static DELIVERY_PRICE = 150
+  static #BONUS_FACTOR = 0.1
+
   static #count = 0
   static #list = []
+
+  static #bonusAccount = new Map()
+
+  static getBonusBalance = (email) => {
+    return Purchase.#bonusAccount.get(email) || 0
+  }
+
+  static calcBonusAmount = (value) => {
+    return value * Purchase.#BONUS_FACTOR
+  }
+
+  static updateBonusBalance = (
+    email,
+    price,
+    bonusUse = 0,
+  ) => {
+    const amount = this.calcBonusAmount(price)
+
+    const currentBalance = Purchase.getBonusBalance(email)
+
+    const updatedBalance =
+      currentBalance + amount - bonusUse
+    Purchase.#bonusAccount.set(email, updatedBalance)
+
+    console.log(email, updatedBalance)
+
+    return amount
+  }
 
   constructor(data, product) {
     this.id = ++Purchase.#count
@@ -87,7 +117,11 @@ class Purchase {
   }
 
   static getList = () => {
-    return Purchase.#list.reverse()
+    return Purchase.#list
+      .reverse()
+      .map(({ id, title, totalPrice, bonus }) => {
+        return { id, title, totalPrice, bonus }
+      })
   }
 
   static getById = (id) => {
@@ -110,11 +144,39 @@ class Purchase {
     }
   }
 }
+
+class Promocode {
+  static #list = []
+
+  constructor(name, factor) {
+    this.name = name
+    this.factor = factor
+  }
+
+  static add = (name, factor) => {
+    const newPromoCode = new Promocode(name, factor)
+    Promocode.#list.push(newPromoCode)
+    return newPromoCode
+  }
+
+  static getByName = (name) => {
+    return this.#list.find((promo) => promo.name === name)
+  }
+
+  static calc = (promo, price) => {
+    return price * promo.factor
+  }
+}
+
+Promocode.add('SUMMER2024', 0.9)
+Promocode.add('DISCOUNT50', 0.5)
+Promocode.add('SALE25', 0.75)
+
 // ================================================================
 
 Product.add(
   'https://picsum.photos/200/300',
-  "Комп'ютер AMD Ryzen 5 3600",
+  "Комп'ютер Intel(R) Core(TM) i5-11800H",
 
   'AMD Ryzen 5 3600 (3.6 - 4.2 ГГц) / RAN 16 ГБ / HDD 1ТБ + SSD 480 Гб / nVidia GeForce RTX 3050, 6гб / без ОД / LAN / без ОС ',
   [
@@ -127,7 +189,7 @@ Product.add(
 
 Product.add(
   'https://picsum.photos/200/300',
-  "Комп'ютер AMD Ryzen 5 3600",
+  "Комп'ютер Intel(R) Core(TM) i7-11800H",
 
   'AMD Ryzen 5 3600 (3.6 - 4.2 ГГц) / RAN 16 ГБ / HDD 1ТБ + SSD 480 Гб / nVidia GeForce RTX 3090, 12гб / без ОД / LAN / без ОС ',
   [{ id: 2, text: 'Топ продажів' }],
@@ -172,10 +234,6 @@ router.post('/purchase-create', function (req, res) {
   const id = Number(req.query.id)
   const amount = Number(req.body.amount)
 
-  console.log(`(1) ${amount}`)
-
-  // console.log(id, amount)
-
   if (amount < 1) {
     return res.render('alert', {
       style: 'alert',
@@ -206,6 +264,7 @@ router.post('/purchase-create', function (req, res) {
 
   const productPrice = product.price * amount
   const totalPrice = productPrice + Purchase.DELIVERY_PRICE
+  const bonus = Purchase.calcBonusAmount(totalPrice)
 
   res.render('purchase-create', {
     style: 'purchase-create',
@@ -226,6 +285,7 @@ router.post('/purchase-create', function (req, res) {
       productPrice,
       deliveryPrice: Purchase.DELIVERY_PRICE,
       amount,
+      bonus,
     },
   })
 })
@@ -243,11 +303,13 @@ router.post('/purchase-submit', function (req, res) {
     lastname,
     email,
     phone,
+    comment,
+
+    promocode,
+    bonus,
   } = req.body
 
   const product = Product.getById(id)
-
-  console.log(`(1) ${amount}`)
 
   if (!product) {
     return res.render('alert', {
@@ -261,17 +323,23 @@ router.post('/purchase-submit', function (req, res) {
     })
   }
 
+  if (product.amount < amount) {
+    return res.render('alert', {
+      style: 'alert',
+
+      data: {
+        success: 'Невдале виконання дії',
+        info: 'Товару немає в потрібній кількості',
+        link: '/purchase-list',
+      },
+    })
+  }
+
   totalPrice = Number(totalPrice)
   productPrice = Number(productPrice)
   deliveryPrice = Number(deliveryPrice)
   amount = Number(amount)
-
-  // console.log(
-  //   totalPrice,
-  //   productPrice,
-  //   deliveryPrice,
-  //   amount,
-  // )
+  bonus = Number(bonus)
 
   if (
     isNaN(totalPrice) ||
@@ -302,6 +370,31 @@ router.post('/purchase-submit', function (req, res) {
     })
   }
 
+  if (bonus || bonus > 0) {
+    const bonusAmount = Purchase.getBonusBalance(email)
+
+    console.log(bonusAmount)
+
+    if (bonus > bonusAmount) {
+      bonus = bonusAmount
+    }
+
+    Purchase.updateBonusBalance(email, totalPrice, bonus)
+
+    totalPrice -= bonus
+  } else {
+    Purchase.updateBonusBalance(email, totalPrice, 0)
+  }
+
+  if (promocode) {
+    promocode = Promocode.getByName(promocode)
+
+    if (promocode)
+      totalPrice = Promocode.calc(promocode, totalPrice)
+  }
+
+  if (totalPrice < 0) totalPrice = 0
+
   const purchase = Purchase.add(
     {
       totalPrice,
@@ -313,6 +406,7 @@ router.post('/purchase-submit', function (req, res) {
       lastname,
       email,
       phone,
+      comment,
     },
     product,
   )
@@ -326,6 +420,20 @@ router.post('/purchase-submit', function (req, res) {
       success: 'Успішне виконання дії',
       info: 'Замовлення створено',
       link: '/purchase-list',
+    },
+  })
+})
+
+router.get('/purchase-list', function (req, res) {
+  const purchase = Purchase.getList()
+
+  console.log(purchase)
+
+  res.render('purchase-list', {
+    style: 'purchase-list',
+
+    data: {
+      list: purchase,
     },
   })
 })
